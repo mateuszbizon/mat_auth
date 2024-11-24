d2e82d5f050907330a81aabef0d9869875ff6e2b
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-11-24 18:41:44 +0100

    sign up schema

---
+++ server/src/constants/validations.ts
@@ -0,0 +1,14 @@
+export const USER_NAME_MIN_LENGTH = 1
+export const USER_NAME_LENGTH_MESSAGE = "Name length can't be empty"
+
+export const USERNAME_MIN_LENGTH = 1
+export const USERNAME_MAX_LENGTH = 5
+export const USERNAME_LENGTH_MESSAGE = `Length of username must be between ${USERNAME_MIN_LENGTH} and ${USERNAME_MAX_LENGTH}`
+export const USERNAME_WHITESPACE_MESSAGE = "Username can't include any whitespaces"
+
+export const PASSWORD_MIN_LENGTH = 5
+export const PASSWORD_LENGTH_MESSAGE = `Password length must be minimum ${PASSWORD_MIN_LENGTH}`
+
+export const EMAIL_MESSAGE = "Invalid email"
+
+export const NO_WHITESPACE_REGEX = /\s/
