44766f3b30f242474b10ce9400945c5b17505c69
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-11-24 15:32:44 +0100

    init commit

---
+++ server/src/index.ts
@@ -0,0 +1,16 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+import bodyParser from "body-parser"
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 3001
+
+app.use(bodyParser.json())
+app.use(cors())
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
