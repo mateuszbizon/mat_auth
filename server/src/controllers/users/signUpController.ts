85c7549235d31b552d7e38309e3fb039f45ffa2f
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-11-25 10:58:52 +0100

    user routes

---
+++ server/src/controllers/users/signUpController.ts
@@ -0,0 +1,9 @@
+import { NextFunction, Request, Response } from "express";
+
+export async function signUpController(req: Request, res: Response, next: NextFunction) {
+    try {
+        
+    } catch (error) {
+        console.error(error)
+    }
+}
