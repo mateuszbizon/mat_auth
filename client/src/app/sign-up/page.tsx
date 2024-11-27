79b2a353e21a48f501c470f4698d800b87c8be88
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-11-27 13:55:16 +0100

    sign up form

--- client/src/app/sign-up/page.tsx
+++ client/src/app/sign-up/page.tsx
@@ -1,9 +1,11 @@
+import SignUpForm from '@/components/forms/SignUpForm'
 import React from 'react'
 
 function SignUpPage() {
   return (
-    <div>
-        
+    <div className='w-full max-w-[600px] mx-auto rounded-xl bg-card p-5 border-2 border-border'>
+        <h1 className='text-2xl text-center text-primary'>Sign Up</h1>
+        <SignUpForm />
     </div>
   )
 }
---
+++ client/src/components/forms/SignUpForm.tsx
@@ -0,0 +1,91 @@
+"use client"
+
+import { signUpSchema, TSignUpSchema } from '@/validations/signUpSchema'
+import { zodResolver } from '@hookform/resolvers/zod'
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '../ui/form'
+import { Input } from '../ui/input'
+import { Button } from '../ui/button'
+
+function SignUpForm() {
+    const form = useForm<TSignUpSchema>({
+        resolver: zodResolver(signUpSchema),
+        defaultValues: {
+            username: "",
+            name: "",
+            email: "",
+            password: "",
+        }
+    })
+
+    function onSubmit(data: TSignUpSchema) {
+        console.log(data)
+    }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input placeholder="Name" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+                <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                    <Input placeholder="Username" {...field} />
+                </FormControl>
+                <FormMessage />
+                </FormItem>
+            )}
+        />
+
+        <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+                <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                    <Input placeholder="Email" {...field} />
+                </FormControl>
+                <FormMessage />
+                </FormItem>
+            )}
+        />
+
+        <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+                <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                    <Input type='password' placeholder="Password" {...field} />
+                </FormControl>
+                <FormMessage />
+                </FormItem>
+            )}
+        />
+
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  )
+}
+
+export default SignUpForm
