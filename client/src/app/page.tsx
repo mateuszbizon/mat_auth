1811cf9e1b1eba8b3d1eef872c42194a7fd08985
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-11-27 12:18:56 +0100

    sign up page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,19 @@
+import Link from "next/link";
+import { buttonVariants } from "@/components/ui/button"
+
 export default function Home() {
   return (
-    <h1 className="text-3xl text-center text-primary">Mat auth project to test authentication in React</h1>
+    <div>
+      <h1 className="text-3xl text-center text-primary">Mat auth project to test authentication in React</h1>
+      <div className="flex justify-center space-x-5 flex-wrap mt-5">
+        <Link href={"/sign-in"} className={buttonVariants({ variant: "outline" })} title="Sign In">
+          Sign In
+        </Link>
+
+        <Link href={"/sign-up"} className={buttonVariants({ variant: "secondary" })} title="Sign Up">
+          Sign Up
+        </Link>
+      </div>
+    </div>
   );
 }
