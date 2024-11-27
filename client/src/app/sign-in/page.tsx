0becee4cc4ccb91be995e557ff88a8fd091c5ed2
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-11-27 18:24:03 +0100

    sign in page

---
+++ client/src/app/sign-in/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SignInPage() {
+  return (
+    <div className='w-full max-w-[600px] mx-auto rounded-xl bg-card p-5 border-2 border-border'>
+        <h1 className='text-2xl text-center text-primary'>Sign In</h1>     
+    </div>
+  )
+}
+
+export default SignInPage
