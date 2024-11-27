af909122159e3ddce121d12c67b811b72e7a6243
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-11-27 11:41:36 +0100

    setup shadcn

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl text-center">Mat auth project to test authentication in React</h1>
+    <h1 className="text-3xl text-center text-primary">Mat auth project to test authentication in React</h1>
   );
 }
