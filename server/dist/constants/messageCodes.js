a13d92148f2001b9e16354ea3a30aab164411137
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-11-29 19:03:46 +0100

    deleted dist

--- server/dist/constants/messageCodes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MESSAGE_CODES = void 0;
-exports.MESSAGE_CODES = {
-    validationFail: "VALIDATION_FAIL",
-    usernameTaken: "USERNAME_TAKEN",
-    emailTaken: "EMAIL_TAKEN",
-    invalidCredentials: "INVALID_CREDENTIALS"
-};
--- server/dist/dtos/signInSchema.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.signInSchema = void 0;
-const zod_1 = require("zod");
-exports.signInSchema = zod_1.z.object({
-    username: zod_1.z.string().min(1, "Username can't be empty"),
-    password: zod_1.z.string().min(1, "Password can't be empty"),
-});
