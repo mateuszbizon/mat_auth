4e9d18ec0293a5fc214de33b46dd902c2301cca6
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2024-11-29 15:12:42 +0100

    counter redux values

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -2,10 +2,16 @@
 
 import { Button } from '@/components/ui/button'
 import useGetUsers from '@/hooks/services/users/useGetUsers'
+import { AppDispatch, RootState } from '@/lib/store'
+import { decrement, increment, setValue } from '@/lib/store/slices/signInSlice'
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { useSelector } from 'react-redux'
 
 function DashboardPage() {
     const { users, handleGetUsers, isPending } = useGetUsers()
+    const counter = useSelector((state: RootState) => state.signIn)
+    const dispatch = useDispatch<AppDispatch>()
 
     function onGetUsers() {
         handleGetUsers()
@@ -13,6 +19,22 @@ function DashboardPage() {
 
   return (
     <div>
+        <div className='flex flex-col items-center space-y-3 mb-10'>
+            <div className='flex flex-wrap justify-center gap-5'>
+                <Button onClick={() => dispatch(increment())}>
+                    Increment
+                </Button>
+
+                <Button onClick={() => dispatch(decrement())}>
+                    Decrement
+                </Button>
+
+                <Button onClick={() => dispatch(setValue(10))}>
+                    Set value to 10
+                </Button>
+            </div>
+            <span>{counter.value}</span>
+        </div>
         <div className='flex justify-center'>
             <Button variant={"destructive"} onClick={onGetUsers}>
                 {isPending ? "Loading..." : "Get users"}
